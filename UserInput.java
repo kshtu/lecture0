251e039843439cd4b417c89b0619ed16cfca2c7e
kshtu <42929621+kshtu@users.noreply.github.com>
2018-09-06 11:27:31 +0530

    JavaFile

---
+++ UserInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class UserInput
+{
+public static void main(String []args)
+{
+Scanner input=new Scanner(System.in);
+System.out.println("Enter two numbers");
+int a=input.nextInt();
+int b=input.nextInt();
+int c=a+b;
+System.out.println("The Sum Of " +a+ " and " +b+ " is "+c);
+}}
